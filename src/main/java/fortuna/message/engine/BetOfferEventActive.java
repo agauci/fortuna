100a518bc1c1d872dc3425f8e8d5586f710e58c0
Andrew Gauci <gauciandrew@gmail.com>
2023-05-07 00:24:37 +0200

    tweaks

---
+++ src/main/java/fortuna/message/engine/BetOfferEventActive.java
@@ -0,0 +1,17 @@
+package fortuna.message.engine;
+
+import fortuna.models.competition.EventCompetition;
+import fortuna.models.source.BettingSourceType;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Builder
+@Data
+public class BetOfferEventStarted implements BetEventMessage {
+    BettingSourceType   source;
+    String              eventIdentifier;
+    List<String>        participants;
+    EventCompetition    eventCompetition;
+}
